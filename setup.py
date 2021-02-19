daf1d76a392d41b878e9e552e0bef3120fd71859
pcko1 <kotsias.pan@gmail.com>
2021-02-19 12:48:41 +0100

    Fixed subpackage names

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup
 
 setup(
-    name="bscscanscan-python",
+    name="bscscan-python",
     version="2.0.2",
     description="A python API for bscscan.com.",
-    url="https://github.com/pcko1/bscscanscan-python",
+    url="https://github.com/pcko1/bscscan-python",
     author="Panagiotis-Christos Kotsias",
     author_email="kotsias.pan@gmail.com",
     license="MIT",
     packages=[
-        "bscscanscan",
-        "bscscanscan.configs",
-        "bscscanscan.enums",
-        "bscscanscan.modules",
-        "bscscanscan.utils",
+        "bscscan",
+        "bscscan.configs",
+        "bscscan.enums",
+        "bscscan.modules",
+        "bscscan.utils",
     ],
     install_requires=["requests", "coverage"],
     include_package_data=True,
