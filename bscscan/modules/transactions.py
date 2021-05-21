ca9a725b52a4c0f9097ecd6458f2514bd4c1c145
pcko1 <kotsias.pan@gmail.com>
2021-05-21 15:14:40 +0200

    Updated docs

--- bscscan/modules/transactions.py
+++ bscscan/modules/transactions.py
@@ -12,7 +12,9 @@ class Transactions:
             txhash (str): Target tx hash.
 
         Returns:
-            str: The status code.::
+            str: The status code.
+
+        Example:::
 
                 get_tx_receipt_status(
                     txhash="0xe9975702518c79caf81d5da65dea689dcac701fcdd063f848d4f03c85392fd00"
--- build_docs.sh
+++ build_docs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 clear
+rm -r build/*
 sphinx-apidoc -f -o source bscscan
 make html
