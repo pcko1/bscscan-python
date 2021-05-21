97ce575cc65a9eeeed8d2d7030f4185edfe97293
pcko1 <kotsias.pan@gmail.com>
2021-05-21 14:45:16 +0200

    Added docstrings

--- bscscan/modules/transactions.py
+++ bscscan/modules/transactions.py
@@ -6,7 +6,24 @@ from bscscan.enums.modules_enum import ModulesEnum as modules
 class Transactions:
     @staticmethod
     def get_tx_receipt_status(txhash: str):
-        url = (
+        """Chect the status of a transaction receipt.
+
+        Args:
+            txhash (str): Target tx hash.
+
+        Returns:
+            str: The status code.
+
+        Example:
+            Args:
+                get_tx_receipt_status(
+                    txhash="0xe9975702518c79caf81d5da65dea689dcac701fcdd063f848d4f03c85392fd00"
+                )
+
+            Returns:
+                "1"
+        """
+        return (
             f"{fields.MODULE}"
             f"{modules.TRANSACTION}"
             f"{fields.ACTION}"
@@ -14,4 +31,3 @@ class Transactions:
             f"{fields.TXHASH}"
             f"{txhash}"
         )
-        return url
