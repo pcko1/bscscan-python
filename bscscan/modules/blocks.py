4c8e13236bae1406b17df2a0322b018a7c8ea52f
pcko1 <kotsias.pan@gmail.com>
2021-05-21 14:02:30 +0200

    Added docstrings

--- bscscan/modules/blocks.py
+++ bscscan/modules/blocks.py
@@ -5,8 +5,32 @@ from bscscan.enums.modules_enum import ModulesEnum as modules
 
 class Blocks:
     @staticmethod
-    def get_block_reward_by_block_number(block_no: str):
-        url = (
+    def get_block_reward_by_block_number(block_no: int):
+        """Get block reward by block number.
+
+        Args:
+            block_no (int): Target block number.
+
+        Returns:
+            dict: Block reward as a dictionary of various data.
+
+        Example:
+            Args:
+                get_block_reward_by_block_number(
+                    block_no="2150000"
+                )
+
+            Returns:
+                {
+                    "blockNumber": "2150000",
+                    "timeStamp": "1605122780",
+                    "blockMiner": "0xee01c3b1283aa067c58eab4709f85e99d46de5fe",
+                    "blockReward": "12141249999983048",
+                    "uncles": [],
+                    "uncleInclusionReward": "0"
+                }
+        """
+        return (
             f"{fields.MODULE}"
             f"{modules.BLOCK}"
             f"{fields.ACTION}"
@@ -14,11 +38,32 @@ class Blocks:
             f"{fields.BLOCKNO}"
             f"{block_no}"
         )
-        return url
 
     @staticmethod
-    def get_est_block_countdown_time_by_block_number(block_no: str):
-        url = (
+    def get_est_block_countdown_time_by_block_number(block_no: int):
+        """Get estimated countdown time for a given block.
+
+        Args:
+            block_no (int): Target block number.
+
+        Returns:
+            dict: Countdown time in a dictionary of various data.
+
+        Example:
+            Args:
+                get_est_block_countdown_time_by_block_number(
+                    block_no="8000000"
+                )
+
+            Returns:
+                {
+                    "CurrentBlock": "7584646",
+                    "CountdownBlock": "8000000",
+                    "RemainingBlock": "415354",
+                    "EstimateTimeInSec": "1246077.0"
+                }
+        """
+        return (
             f"{fields.MODULE}"
             f"{modules.BLOCK}"
             f"{fields.ACTION}"
@@ -26,12 +71,32 @@ class Blocks:
             f"{fields.BLOCKNO}"
             f"{block_no}"
         )
-        return url
 
     @staticmethod
     def get_block_number_by_timestamp(timestamp: int, closest: str):
-        # NOTE: Supports UNIX timestamps in seconds
-        url = (
+        """Get block number given a specific timestamp.
+
+        NOTE: Supports UNIX timestamps in seconds.
+
+        Args:
+            timestamp (int): Target timestamp.
+            closest (str): Closest block "before" or "after" target timestamp.
+
+        Returns:
+            str: Block number for requested timestamp.
+
+        Example:
+            Args:
+                get_block_number_by_timestamp(
+                    timestamp="1601510400",
+                    closest="before"
+                )
+
+            Returns:
+                "946206"
+        """
+        assert closest in ["before", "after"], "Specify 'before' or 'after'."
+        return (
             f"{fields.MODULE}"
             f"{modules.BLOCK}"
             f"{fields.ACTION}"
@@ -41,4 +106,3 @@ class Blocks:
             f"{fields.CLOSEST}"
             f"{closest}"
         )
-        return url
